2a6b2fe66858e5696c961cd892f585fd5cc52f68
wilhelm steins <wilhelmsteins@gmail.com>
2022-11-20 16:00:29 -0300

    iniciando component header / correção de ng add @angular/material

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/template/header/header.component';
 
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
+import { MatToolbarModule } from "@angular/material/toolbar" ;
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +15,9 @@ import { HeaderComponent } from './components/template/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
