dd3e13f72f6a04981ed6b5ba840852c5713d8afd
wilhelm steins <wilhelmsteins@gmail.com>
2022-11-21 00:39:10 -0300

    criando component update

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -30,6 +30,7 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import localePt from "@angular/common/locales/pt"
 import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 registerLocaleData(localePt)
 @NgModule({
@@ -44,6 +45,7 @@ registerLocaleData(localePt)
     ForDirective,
     ProductCreateComponent,
     ProductReadComponent,
+    ProductUpdateComponent,
   ],
   imports: [
     BrowserModule,
